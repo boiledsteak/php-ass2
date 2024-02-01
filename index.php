19c74a6eff9853876552e62ba1b5c27390275d49
boiledsteak <timothymxa@gmail.com>
2024-02-01 10:48:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- no need password -->
+<!-- minimum do check in check out -->
+<!-- due 19 feb -->
+<!-- extra features no extra marks -->
+<!-- no delete feature -->
+<!-- SQL statement should create database if no exist. add to db if yes exist -->
+<!-- prepared statement not needed -->
