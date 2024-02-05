77adff82e7a299079144f6acf12184a615ec94bf
boiledsteak <timothymxa@gmail.com>
2024-02-05 14:43:54 +0800

    can add user

--- index.php
+++ index.php
@@ -13,20 +13,76 @@ $request = strtolower($_SERVER['REQUEST_URI']);
 $viewDir = '/pages/';
 $musicDir = '/pics/';
 
+$servername = "localhost";  
+$username = "root";      
+$password = "";      
+$dbname = "php-ass2";
+
+
+function registerComponent()
+{
+    echo '
+        <div class="canvas">
+            <div class="mainpagefn">
+                <div class="title">
+                Please enter account details
+                </div>
+                <form class="nameform" method="post" action="/registernew">
+                    <div class="namebox">
+                        <div class="thename">
+                            Username: 
+                        </div>
+                        <input type="text" id="fname" name="fname" required>
+                    </div>
+                    <div class="namebox">
+                        <div class="thename">
+                            Surname: 
+                        </div>
+                        <input type="text" id="fname" name="fsname" required>
+                    </div>
+                    <div class="namebox">
+                        <div class="thename">
+                            Role: 
+                        </div>
+                        <input type="text" id="fname" name="frole" required>
+                    </div>
+                    <div class="namebox">
+                        <div class="thename">
+                            Email: 
+                        </div>
+                        <input type="text" id="fname" name="femail" required>
+                    </div>
+                    <div class="namebox">
+                        <div class="thename">
+                            Phone number: 
+                        </div>
+                        <input type="text" id="fname" name="fphone" required>
+                    </div>
+                    <div class="optionsbox">
+                        <button type="submit" class="options">
+                        Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    ';
+}
+
 function headerComponent()
 {
     echo '
     <div class="outerhead">
         <div class="thehead">
             <a class="thelogo" href="/">
-                Funny facts
+                Easy Parking
             </a>
             <div class="menoptions">
                 <div class="menoption">
-                    <a href="/leaderboard">Leaderboard</a>
+                    <a href="/register">Register</a>
                 </div>
                 <div class="menoption">
-                    <a href="/exit">Exit</a>
+                    <a href="/login">Login</a>
                 </div>
             </div>
         </div>
@@ -53,6 +109,66 @@ switch ($request) {
         break;
     }
 
+    case '/css':
+    {
+        header('Content-Type: text/css');
+        require __DIR__ . '/main.css';
+        break;
+    }
+
+    case '/register':
+    {
+        require __DIR__ . $viewDir . 'mainpage.php';
+        headerComponent();
+        registerComponent();
+        btmComponent();      
+        break;
+    }
+
+    case '/registernew':
+    {
+        // Create a connection to the database
+        try 
+        {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // Set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Check if the form is submitted
+            if ($_SERVER["REQUEST_METHOD"] == "POST") 
+            {
+
+                // Get data from the form
+                $name = strtolower(htmlspecialchars($_POST['fname']));
+                $sname = strtolower(htmlspecialchars($_POST['fsname']));
+                $role = strtolower(htmlspecialchars($_POST['frole']));
+                $email = strtolower(htmlspecialchars($_POST['femail']));
+                $phone = intval($_POST['fphone']);  
+
+                // SQL statement to insert data into the usertable
+                $sql = "INSERT INTO usertable (name, role, surname, email, phone) VALUES (:name, :role, :sname, :email, :phone)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':name', $name);
+                $stmt->bindParam(':role', $role);
+                $stmt->bindParam(':sname', $sname);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':phone', $phone);
+
+                // Execute the statement
+                $stmt->execute();
+
+                echo "New record created successfully";
+            }
+        } 
+        catch (PDOException $e) 
+        {
+            // put into error_log when ready
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+        break;
+    }
+
     default:
     {
         http_response_code(404);
@@ -61,5 +177,6 @@ switch ($request) {
         exit;
     }
 }
-
+// Close the connection
+$conn = null;
 ?>
