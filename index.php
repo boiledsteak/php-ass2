63783e959aff734e9377f47593f62fd11bef36e3
boiledsteak <timothymxa@gmail.com>
2024-02-05 10:47:58 +0800

    added skeleton

--- index.php
+++ index.php
@@ -5,3 +5,61 @@
 <!-- no delete feature -->
 <!-- SQL statement should create database if no exist. add to db if yes exist -->
 <!-- prepared statement not needed -->
+
+<?php
+session_start();
+
+$request = strtolower($_SERVER['REQUEST_URI']);
+$viewDir = '/pages/';
+$musicDir = '/pics/';
+
+function headerComponent()
+{
+    echo '
+    <div class="outerhead">
+        <div class="thehead">
+            <a class="thelogo" href="/">
+                Funny facts
+            </a>
+            <div class="menoptions">
+                <div class="menoption">
+                    <a href="/leaderboard">Leaderboard</a>
+                </div>
+                <div class="menoption">
+                    <a href="/exit">Exit</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    ';
+}
+
+function btmComponent()
+{
+    echo '
+    <div class="btm"></div>
+    ';
+}
+
+// Router API
+switch ($request) {
+
+    case '/':
+    {
+        require __DIR__ . $viewDir . 'mainpage.php';
+        headerComponent();
+        
+        btmComponent();      
+        break;
+    }
+
+    default:
+    {
+        http_response_code(404);
+        $errorPageUrl = "https://http.cat/404";
+        header("Location: $errorPageUrl");
+        exit;
+    }
+}
+
+?>
