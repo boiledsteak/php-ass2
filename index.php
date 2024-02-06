c5ee969550167ecee06ec6de3f1458451f805204
boiledsteak <timothymxa@gmail.com>
2024-02-06 11:45:17 +0800

    parking location admin side done

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ function adminComponent()
         echo '<div class="canvas"><div class="mainpagefn">';
         echo '<div class="title">Welcome, ' . $currentUserName . '!</div>';
 
-        // Search form
+        // Search user form
         echo '<div class="searchcon"><form method="post" action="/">';
         echo '<label for="searchUser">Search for user:</label>';
         echo '<div><input type="text" id="searchUser" name="searchUser">';
@@ -62,45 +62,31 @@ function adminComponent()
         {
             displayAllUsers();
         }
-
-        
-
-        echo '
-            <div class="abilitybox">
-                <div class="abilities">
-                    Parking Locations
-                </div>
-                <form class="plocform" method="post" action="/insertparkingloc">
-                <div class="form-row">
-                    <label for="locName">Location Name:</label>
-                    <input type="text" id="locName" name="locName" required>
-                </div>
-
-                <div class="form-row">
-                    <label for="locDescription">Description:</label>
-                    <input type="text" id="locDescription" name="locDescription">
-                </div>
-
-                <div class="form-row">
-                    <label for="locCapacity">Capacity:</label>
-                    <input type="number" id="locCapacity" name="locCapacity" required>
-                </div>
-
-                <div class="form-row">
-                    <label for="locCostPerHour">Cost Per Hour:</label>
-                    <input type="text" id="locCostPerHour" name="locCostPerHour">
-                </div>
-
-                <div class="form-row">
-                    <label for="locCostPerHourLateCheckOut">Cost Per Hour Late Check Out:</label>
-                    <input type="text" id="locCostPerHourLateCheckOut" name="locCostPerHourLateCheckOut">
-                </div>
-
-                    <button class="nicebtn" type="submit">Add Parking Location</button>
-                </form>
-
-            </div>
-        ';
+        insertPloc();
+        // Search form
+        echo '<div class="searchcon"><form method="post" action="/">';
+        echo '<label for="searchPloc">Search for location:</label>';
+        echo '<div><input type="text" id="searchPloc" name="searchPloc">';
+        echo '<button class="nicebtn" type="submit">Go</button></div>';
+        echo '</form></div>';
+        // Display search results or all parking locations if form not submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['searchPloc'])) 
+        {
+            $searchLocationName = strtolower(htmlspecialchars($_POST['searchPloc']));
+            // If search input is empty, display all parking locations
+            if (empty($searchLocationName)) 
+            {
+                displayAllPlocs();
+            } 
+            else 
+            {
+                searchForParkingLocation($searchLocationName);
+            }
+        } 
+        else 
+        {
+            displayAllPlocs();
+        }
         echo '</div></div>';
     } 
     else 
@@ -195,6 +181,131 @@ function searchForUser($searchUserName)
     }
 }
 
+function insertPloc()
+{
+    echo '
+            <div class="abilitybox">
+                <div class="abilities">
+                    Add Parking Location
+                </div>
+                <form class="plocform" method="post" action="/insertparkingloc">
+                <div class="form-row">
+                    <label for="locName">Location Name:</label>
+                    <input type="text" id="locName" name="locName" required>
+                </div>
+
+                <div class="form-row">
+                    <label for="locDescription">Description:</label>
+                    <input type="text" id="locDescription" name="locDescription">
+                </div>
+
+                <div class="form-row">
+                    <label for="locCapacity">Capacity:</label>
+                    <input type="number" id="locCapacity" name="locCapacity" required>
+                </div>
+
+                <div class="form-row">
+                    <label for="locCostPerHour">Cost Per Hour:</label>
+                    <input type="text" id="locCostPerHour" name="locCostPerHour">
+                </div>
+
+                <div class="form-row">
+                    <label for="locCostPerHourLateCheckOut">Cost Per Hour Late Check Out:</label>
+                    <input type="text" id="locCostPerHourLateCheckOut" name="locCostPerHourLateCheckOut">
+                </div>
+
+                    <button class="nicebtn" type="submit">Add Parking Location</button>
+                </form>
+
+            </div>
+        ';
+}
+
+function displayAllPlocs()
+{
+    global $servername, $username, $password, $dbname;
+
+    try 
+    {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->query("SELECT * FROM parkinglocs");
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($users) 
+        {
+            echo '<div class="abilitybox"><div class="abilities" >All Parking Locations</div>';
+            echo '<table border="1">';
+            echo '<tr><th>ID</th><th>Location</th><th>Description</th><th>Capacity</th><th>CostPerHour</th><th>Late rate</th></tr>';
+            
+            foreach ($users as $user) 
+            {
+                echo '<tr>';
+                echo '<td>' . $user['ParkingID'] . '</td>';
+                echo '<td>' . $user['Location'] . '</td>';
+                echo '<td>' . $user['Description'] . '</td>';
+                echo '<td>' . $user['Capacity'] . '</td>';
+                echo '<td>' . $user['CostPerHour'] . '</td>';
+                echo '<td>' . $user['CostPerHourLateCheckOut'] . '</td>';
+                echo '</tr>';
+            }
+            
+            echo '</table></div>';
+        } 
+        else 
+        {
+            echo '<p>No users found.</p>';
+        }
+    } 
+    catch (PDOException $e) 
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+
+function searchForParkingLocation($searchLocationName)
+{
+    global $servername, $username, $password, $dbname;
+
+    try 
+    {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // SQL statement to search for a specific parking location
+        $sql = "SELECT * FROM parkinglocs WHERE Location = :searchLocationName";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':searchLocationName', $searchLocationName);
+        $stmt->execute();
+
+        $location = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($location) 
+        {
+            echo '<div class="abilitybox"><div class="abilities">'.$location['Location'].'</div>';
+            echo '<table border="1">';
+            echo '<tr><th>ID</th><th>Location</th><th>Description</th><th>Capacity</th><th>Cost Per Hour</th><th>Cost Per Hour Late Check Out</th></tr>';
+            echo '<tr>';
+            echo '<td>' . $location['ParkingID'] . '</td>';
+            echo '<td>' . $location['Location'] . '</td>';
+            echo '<td>' . $location['Description'] . '</td>';
+            echo '<td>' . $location['Capacity'] . '</td>';
+            echo '<td>' . $location['CostPerHour'] . '</td>';
+            echo '<td>' . $location['CostPerHourLateCheckOut'] . '</td>';
+            echo '</tr>';
+            echo '</table></div>';
+        } 
+        else 
+        {
+            echo '<p>No parking location found with the name ' . $searchLocationName . '.</p>';
+        }
+    } 
+    catch (PDOException $e) 
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
 
 function loginComponent()
 {
